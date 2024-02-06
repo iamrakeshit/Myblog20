19ed89a7b86e7eec8fcb4c4fa1eb3407b06a9c98
iamrakeshit <iamrakeshit273@gmail.com>
2024-02-06 23:51:42 +0530

    127:Blog 20 created

---
+++ src/main/java/com/myblog/myblog20/Myblog20Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog20/Myblog20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
